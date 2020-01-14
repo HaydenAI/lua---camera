87fb4e2fc074db1fd89c9e79388ca54af11d6724
maym86 <maym86@gmail.com>
2020-01-14 15:55:40 -0800

    update installer and exampel

--- camera-1.0-0.rockspec
+++ camera-1.0-0.rockspec
@@ -30,5 +30,5 @@ cd build;
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(LUA_BINDIR)/.." -DCMAKE_INSTALL_PREFIX="$(PREFIX)"; 
 $(MAKE)
    ]],
-   install_command = "cd build && sudo $(MAKE) install"
+   install_command = "cd build && $(MAKE) install"
 }
--- examples/test-one.lua
+++ examples/test-one.lua
@@ -1,14 +1,14 @@
 require 'torch'
 require 'sys'
 require 'camera'
-width  = 1600
-height = 896
-fps = 1
+width  = 640
+height = 480
+fps = 30
 dir = "demo_test"
 
 sys.execute(string.format('mkdir -p %s',dir))
 
-camera1 = image.Camera{idx=1,width=width,height=height,fps=fps}
+camera1 = image.Camera{idx=0,width=width,height=height,fps=fps}
 
 a1 = camera1:forward()
 win = image.display{win=win,image={a1}}
@@ -18,7 +18,7 @@ while true do
    sys.tic()
    a1 = camera1:forward()
    image.display{win=win,image={a1}}
-   image.savePNG(string.format("%s/frame_1_%05d.png",dir,f),a1)
+   --image.savePNG(string.format("%s/frame_1_%05d.png",dir,f),a1)
    f = f + 1
    print("FPS: ".. 1/sys.toc()) 
 end
