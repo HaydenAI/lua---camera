198dd4e3d761ae6cb5f5c2f807e9b2aae7dc794a
maym86 <maym86@gmail.com>
2020-01-14 16:54:25 -0800

    opencv true

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ require 'xlua'
 ----------------------------------
 -- load camera driver based on OS
 ----------------------------------
+useOpenCV = true
 if useOpenCV then 
    if not xlua.require 'camopencv' then
       xlua.error('failed to load camopencv wrapper: verify that camopencv is installed')
